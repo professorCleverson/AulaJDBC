73c96e594acac4e5251ec1be5fdf6e0772a86080
Cleverson <cleverson.silva@msn.com>
2026-05-05 19:24:36 -0400

    .

--- src/TestaAlunoRev01.java
+++ src/TestaAlunoRev01.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class TestaAlunoRev01 {
 	public static void main(String[] args) {
 		AlunoDAO dao = new AlunoDAO();
-		int opcao = 0, resp = 1;
+		int opcao = 0, resp = 1, cod = 0;
 		String nome, cpf;
 		Scanner scInt = new Scanner(System.in);
 		Scanner scString = new Scanner(System.in);
@@ -33,24 +33,27 @@ public class TestaAlunoRev01 {
 		        }
 				break;
 			case 3:
+				//Atualizar
+				System.out.println("Código:");
+				cod = scInt.nextInt();
+				System.out.println("Novo Nome:");
+				nome = scString.nextLine();
+				System.out.println("Novo CPF:");
+				cpf = scString.nextLine();
+				
+				dao.atualizar(cod, nome, cpf);
+				break;
 			case 4:
+				System.out.println("Código:");
+				cod = scInt.nextInt();
+				
+				dao.deletar(cod);
+				break;
 			default:
 				System.out.println("Opção inválida!");
 			}
 			System.out.println("\nContinuar? 1 = Sim / 2 = Não ");
 			resp = scInt.nextInt();
-			
 		}
-		
-        
-
-        
-
-        // Atualizar
-        dao.atualizar(6, "Maria Silva", "123.456.789-90");
-
-        // Deletar
-        dao.deletar(2);
 	}
-
 }
